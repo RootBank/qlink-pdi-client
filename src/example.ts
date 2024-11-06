4354228ea185fab1a8cd7b10b1f03d3c3a60a1fe
Nic Botes <nic@botes.com>
2024-11-06 14:09:49 +0200

    polish

--- src/example.ts
+++ src/example.ts
@@ -2,8 +2,8 @@ import { DeductionType } from "./enums/deduction-type";
 import { PayrollIdentifier } from "./enums/payroll-identifier";
 import { SEPDIFlag } from "./enums/sepdi-flag";
 import { QLinkClient } from "./models/qlink-client";
-import { Configuration } from "./types";
-import { formatDate } from "./utils/date-helpers";
+import { Configuration, SEPDIPayrollDeductionFields } from "./types";
+import { formatDate, IdFromBirthDate } from "./utils/date-helpers";
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -20,19 +20,21 @@ const main = async () => {
 
   await qlink.testConnection();
 
-  // c.queryEmployeeInfo({ employeeNumber: "123" })
+  const employee = await qlink.queryEmployeeInfo({ employeeNumber: "84177942", payrollIdentifier: PayrollIdentifier.PERSAL });
+
   const deductionFields = {
     amount: 10000,
     deductionType: DeductionType.SEPDI_INSURANCE_LIFE,
-    employeeNumber: "123",
+    employeeNumber: "84177942",
     payrollIdentifier: PayrollIdentifier.PERSAL,
     referenceNumber: "my lucky random number",
-    startDate: "",
+    startDate: formatDate(new Date(2024, 12, 1)).ccyyMM01,
     surname: "SURNAME",
     effectiveSalaryMonth: formatDate(new Date(2024, 10, 1)).ccyyMM,
-    flag: SEPDIFlag.PAPER_MANDATE
-  }
-  // await qlink.createInsurancePayrollDeduction(deductionFields);
+    flag: SEPDIFlag.PAPER_MANDATE,
+    idNumber: IdFromBirthDate(employee.birthDate as string),
+  } as SEPDIPayrollDeductionFields
+  await qlink.createInsurancePayrollDeduction(deductionFields);
 };
 
 main();
