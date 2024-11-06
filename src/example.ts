4636e9980cb95728c2910711e9762390185aee16
Nic Botes <nic@botes.com>
2024-11-06 13:30:37 +0200

    runs exmaple in async func

--- src/example.ts
+++ src/example.ts
@@ -4,29 +4,35 @@ import { SEPDIFlag } from "./enums/sepdi-flag";
 import { QLinkClient } from "./models/qlink-client";
 import { Configuration } from "./types";
 import { formatDate } from "./utils/date-helpers";
-import 'dotenv/config'
+import dotenv from 'dotenv';
 
-const qlink = new QLinkClient(
-  {
-    institution: Number(process.env.QLINK_INSTITUTION_ID),
-    password: process.env.QLINK_PASSWORD,
-    username: process.env.QLINK_USERNAME,
-    baseUrl: process.env.QLINK_URL,
-  } as Configuration
-)
+dotenv.config();
 
-qlink.testConnection();
+const main = async () => {
+  const qlink = new QLinkClient(
+    {
+      institution: Number(process.env.QLINK_INSTITUTION_ID),
+      password: process.env.QLINK_PASSWORD,
+      username: process.env.QLINK_USERNAME,
+      baseUrl: process.env.QLINK_URL,
+    } as Configuration
+  )
 
-// c.queryEmployeeInfo({ employeeNumber: "123" })
-const deductionFields = {
-  amount: 10000,
-  deductionType: DeductionType.SEPDI_INSURANCE_LIFE,
-  employeeNumber: "123",
-  payrollIdentifier: PayrollIdentifier.PERSAL,
-  referenceNumber: "my lucky random number",
-  startDate: "",
-  surname: "SURNAME",
-  effectiveSalaryMonth: formatDate(new Date(2024, 10, 1)).ccyyMM,
-  flag: SEPDIFlag.PAPER_MANDATE
-}
-qlink.createInsurancePayrollDeduction(deductionFields);
+  await qlink.testConnection();
+
+  // c.queryEmployeeInfo({ employeeNumber: "123" })
+  const deductionFields = {
+    amount: 10000,
+    deductionType: DeductionType.SEPDI_INSURANCE_LIFE,
+    employeeNumber: "123",
+    payrollIdentifier: PayrollIdentifier.PERSAL,
+    referenceNumber: "my lucky random number",
+    startDate: "",
+    surname: "SURNAME",
+    effectiveSalaryMonth: formatDate(new Date(2024, 10, 1)).ccyyMM,
+    flag: SEPDIFlag.PAPER_MANDATE
+  }
+  // await qlink.createInsurancePayrollDeduction(deductionFields);
+};
+
+main();
